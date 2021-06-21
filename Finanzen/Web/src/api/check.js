f3a32ace6e0feec8c16d1ad1a9e051f130b58b89
BolverBlitz <bolverblitz@ebg.pw>
2021-06-21 04:33:26 +0200

    WebToken Verification

--- Finanzen/Web/src/api/check.js
+++ Finanzen/Web/src/api/check.js
@@ -5,6 +5,7 @@ useragent = require('express-useragent');
 const Joi = require('joi');
 var path = require('path');
 const DB = require('../lib/postgres');
+const TV = require('../lib/TokenVerification');
 
 
 const PluginConfig = {
@@ -28,58 +29,30 @@ const TokenCheck = Joi.object({
 
 const router = express.Router();
 
-router.post("/check", limiter, async (reg, res, next) => {
+router.post("/", limiter, async (reg, res, next) => {
     try {
-        const value = await LoginCheck.validateAsync(reg.body);
-        DB.get.Guests.ByID(value.userid).then(function(Guest_result) {
-            if(typeof Guest_result[0] === "undefined"){
-                res.status(401);
+        const value = await TokenCheck.validateAsync(reg.body);
+        let source = reg.headers['user-agent']
+        let para = {
+            Browser: useragent.parse(source),
+            IP: reg.headers['x-forwarded-for'] || reg.socket.remoteAddress
+        }
+        TV.check(value.Token, para, false).then(function(Check) {
+            if(Check.State){
+                res.status(200);
                 res.json({
-                  message: "Falscher Benutzername oder Passwort",
+                  TokenData: Check.Data
                 });
             }else{
-                bcrypt.compare(value.password, Guest_result[0].passwort).then(function(result) {
-                    if(result){
-                        let source = reg.headers['user-agent']
-                        let UserAgent = useragent.parse(source)
-                        let IP = reg.headers['x-forwarded-for'] || reg.socket.remoteAddress
-                        let WebToken = randomstring.generate({
-                            length: process.env.WebTokenLength, //DO NOT CHANCE!!!
-                            charset: 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!'
-                        });
-                        //Write new Webtoken
-                        DB.write.webtoken.Add(value.userid, Guest_result[0].username, IP, UserAgent.source, WebToken, Guest_result[0].admin).then(function(WebToken_respone) {
-                            res.status(200);
-                            res.json({
-                            Token: WebToken,
-                            });
-                        }).catch(function(error){
-                            console.log(error)
-                            res.status(500);
-                            res.json({
-                                message: "Kritischer Fehler!",
-                            });
-                        })
-                    }else if(!result){ //Not sure why this is !
-                        res.status(401);
-                        res.json({
-                          message: "Falscher Benutzername oder Passwort",
-                        });
-                      }else{
-                        res.status(500);
-                        res.json({
-                          message: "Kritischer Fehler!",
-                        });
-                      }
-                });
+                DB.del.webtoken.Del(value.Token).then(function(Check) {
+                    console.log(Check.rowCount)
+                    res.status(401);
+                    res.json({
+                        Message: "Token invalid"
+                      });
+                })
             }
-        }).catch(function(error){
-            console.log(error)
-            res.status(500);
-            res.json({
-                message: "Kritischer Fehler!",
-            });
-        })
+        });
     } catch (error) {
         next(error);
     }
