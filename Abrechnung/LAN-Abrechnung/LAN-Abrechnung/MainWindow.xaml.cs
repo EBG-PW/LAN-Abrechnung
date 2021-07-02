e65c2ba241b8399a2a8a1b8f7ea3f505cfc64c4d
Viktor geozukunft <geozukunft@gmail.com>
2021-07-02 02:25:58 +0200

    Starting on Invoice Menu

--- Abrechnung/LAN-Abrechnung/LAN-Abrechnung/MainWindow.xaml.cs
+++ Abrechnung/LAN-Abrechnung/LAN-Abrechnung/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,10 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Microsoft.SqlServer.Server;
+using Npgsql;
+using System.Globalization;
+
 
 namespace LAN_Abrechnung
 {
@@ -20,10 +25,46 @@ namespace LAN_Abrechnung
     /// </summary>
     public partial class MainWindow : Window
     {
+        private DataSet ds = new DataSet();
+        private DataTable dt = new DataTable();
+
+
         public MainWindow()
         {
             InitializeComponent();
+
+            
         }
 
+        
+
+
+        private void Button_OpenInvoice_OnClick(object sender, RoutedEventArgs e)
+        {
+            var InvoiceWindows = new Invoice();
+            InvoiceWindows.Show();
+        }
+
+        public static DataSet GetData(string command)
+        {
+            var connString =
+                "Server = db.geozukunft.at;Port = 6969;User Id = herbert;Password = herbert;Database = lan;";
+
+            IDbConnection dbConnection = new NpgsqlConnection(connString);
+            IDbCommand selectCommand = dbConnection.CreateCommand();
+            selectCommand.CommandText = command;
+            IDbDataAdapter dbDataAdapter = new NpgsqlDataAdapter();
+            dbDataAdapter.SelectCommand = selectCommand;
+
+            DataSet Dset = new DataSet();
+
+            dbDataAdapter.Fill(Dset);
+
+
+
+            return Dset;
+        }
     }
 }
+
+    
