3a3511e3e8c9c45d250fd4fb81491d861177b7e3
Marc <bolverblitz@ebg.pw>
2022-08-04 04:10:58 +0200

    Added subscription based relay for data

--- PlugsServer/src/app.js
+++ PlugsServer/src/app.js
@@ -4,17 +4,23 @@ const app = require('uWebSockets.js').App();
 
 const ControlerCache = require('js-object-cache');
 
-const command = {
+const commandClient = {
   setting: {
-      controler: 'settings_controler',
-      plugs: 'settings_plugs',
-      plugsinfo: 'settings_plug_info'
+    controler: 'settings_controler',
+    plugs: 'settings_plugs',
+    plugsinfo: 'settings_plug_info'
   },
   plug: {
-      power: 'plug_power',
-      status: 'plug_status'
+    power: 'plug_power',
+    status: 'plug_status'
   },
   failed: 'failed'
+}
+const commandWebuser = {
+  plug: {
+    subscribe: 'subscribe_plugid',
+    power: 'plug_power',
+  }
 }
 
 const ReBuildControlerCache = () => {
@@ -63,12 +69,14 @@ app.ws('/client', {
     const { event, data_payload } = message_data;
 
     //If Event is requesting settings of a controler
-    if (event === command.setting.controler) {
+    if (event === commandClient.setting.controler) {
       //Check if the controler is in the cache
       if (ControlerCache.has(data_payload.token)) {
         db.Plugs.GetByControlerID(ControlerCache.get(data_payload.token).controlerid).then(function (Plugs) {
           //Send plugs to the client
-          ws.send(JSON.stringify({ event: command.setting.plugsinfo, data_payload: { plugs: Plugs } }));
+          ws.send(JSON.stringify({ event: commandClient.setting.plugsinfo, data_payload: { plugs: Plugs } }));
+          //Subscribe this websocket to the client ID, so user Websockets can send data to the correct client
+          ws.subscribe(`/client/id/${ControlerCache.get(data_payload.token).controlerid}`);
         });
       } else {
         //Wasn´t in cache, so update it and try again
@@ -76,23 +84,29 @@ app.ws('/client', {
           if (ControlerCache.has(data_payload.token)) {
             db.Plugs.GetByControlerID(ControlerCache.get(data_payload.token).controlerid).then(function (Plugs) {
               //Send plugs to the client
-              ws.send(JSON.stringify({ event: command.setting.plugsinfo, data_payload: { plugs: Plugs } }));
+              ws.send(JSON.stringify({ event: commandClient.setting.plugsinfo, data_payload: { plugs: Plugs } }));
+              //Subscribe this websocket to the client ID, so user Websockets can send data to the correct client
+              ws.subscribe(`/client/id/${ControlerCache.get(data_payload.token).controlerid}`);
             });
           } else {
             //The requested controler dosn´t exist...
-            ws.send(JSON.stringify({ event: command.failed, data_payload: { error: 'No Controler found' } }));
+            ws.send(JSON.stringify({ event: commandClient.failed, data_payload: { error: 'No Controler found' } }));
           }
         }).catch(function (err) {
           console.log(err);
         });
 
       }
-    } else if (event === 'settings_plugs') {
+    } else if (event === commandClient.setting.plugs) {
 
-    } else if (event === 'plug_power') {
+    } else if (event === commandClient.plug.status) {
 
-    } else if (event === 'plug_status') {
-
+    } else if (event === commandClient.plug.power) {
+      //Runs when the client sends status of one plug
+      if (process.env.enable_influx === 'true' || process.env.enable_influx === true) {
+        //Run InfluxDB request
+      }
+      app.publish(`/plug/id/${data_payload.data.ID}`, JSON.stringify({ event: commandWebuser.plug, data_payload: data_payload.data }));
     }
 
     //ws.send(message, isBinary, true);
@@ -108,20 +122,19 @@ app.ws('/webuser', {
   compression: app.DEDICATED_COMPRESSOR_8KB,
 
   open: (ws) => {
-    console.log('WebSocket opened')
-    ws.send('Its open: UwU', (error) => {
-      if (error) {
-        console.log(error)
-      }
-    });
+    log.info(`Opened connection by a webuser`);
   },
 
-  /* For brevity we skip the other events (upgrade, open, ping, pong, close) */
   message: (ws, message, isBinary) => {
-    /* You can do app.publish('sensors/home/temperature', '22C') kind of pub/sub as well */
 
-    /* Here we echo the message back, using compression if available */
-    ws.send(message, isBinary, true);
+    const message_data = JSON.parse(Buffer.from(message).toString());
+    const { event, data_payload } = message_data;
+
+    if (event === commandWebuser.plug.subscribe) {
+      //{"event": "subscribe_plugid", "data_payload": {"plugid": "1"}}
+      log.info(`Subscribing to plug: ${data_payload.plugid}`);
+      ws.subscribe(`/plug/id/${data_payload.plugid}`);
+    }
   }
 
 });
