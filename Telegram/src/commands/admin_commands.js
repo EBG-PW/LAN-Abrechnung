f4e8fb28fa3ec1a35908a62e5a077ba442f2a9ce
Marc <bolverblitz@ebg.pw>
2024-07-02 13:31:55 +0200

    Update admin_commands.js

--- Telegram/src/commands/admin_commands.js
+++ Telegram/src/commands/admin_commands.js
@@ -29,6 +29,23 @@ function executeCommand(command, cwd) {
 }
 
 /**
+ * Replace non supported chars in the PDF generator
+ * @param {String} str 
+ * @returns 
+ */
+const replaceNonSupportedChars = (str) => {
+    str = str.replace(/ä/g, 'ae');
+    str = str.replace(/ö/g, 'oe');
+    str = str.replace(/ü/g, 'ue');
+    str = str.replace(/ß/g, 'ss');
+    str = str.replace(/Ä/g, 'Ae');
+    str = str.replace(/Ö/g, 'Oe');
+    str = str.replace(/Ü/g, 'Ue');
+    str = str.replace(' €', ' Euro');
+    return str;
+}
+
+/**
  * Will generate the config json
  * @param {Array} Guests 
  * @returns 
@@ -57,10 +74,10 @@ function generateJson(Guests, preisliste, mainconfig) {
                 if (kwh_used.rows.length > 0) {
                     sum_cost += Number(kwh_used.rows[0].power_used * preisliste.PauschalKosten.StromKWH.Preis);
                     Items_Array.push({
-                        artikel: newi18n.translate(tglang_response_inner, 'geninvoices.PauschalKosten.kwh'),
-                        priceper: CentToEuro(preisliste.PauschalKosten.StromKWH.Preis).replace(' €', ' Euro'),
+                        artikel: replaceNonSupportedChars(newi18n.translate(tglang_response_inner, 'geninvoices.PauschalKosten.kwh')),
+                        priceper: replaceNonSupportedChars(CentToEuro(preisliste.PauschalKosten.StromKWH.Preis)),
                         amount: kwh_used.rows[0].power_used.toFixed(3).toString().replace('.', ',') + "x",
-                        price: CentToEuro(kwh_used.rows[0].power_used * preisliste.PauschalKosten.StromKWH.Preis).replace(' €', ' Euro'),
+                        price: replaceNonSupportedChars(CentToEuro(kwh_used.rows[0].power_used * preisliste.PauschalKosten.StromKWH.Preis)),
                     })
                 }
 
@@ -70,10 +87,10 @@ function generateJson(Guests, preisliste, mainconfig) {
 
                     sum_cost += Number(preisliste.PauschalKosten[key].Preis);
                     Items_Array.push({
-                        artikel: newi18n.translate(tglang_response_inner, `geninvoices.PauschalKosten.${key}`),
-                        priceper: CentToEuro(preisliste.PauschalKosten[key].Preis).replace(' €', ' Euro'),
+                        artikel: replaceNonSupportedChars(newi18n.translate(tglang_response_inner, `geninvoices.PauschalKosten.${key}`)),
+                        priceper: replaceNonSupportedChars(CentToEuro(preisliste.PauschalKosten[key].Preis)),
                         amount: "1x",
-                        price: CentToEuro(preisliste.PauschalKosten[key].Preis).replace(' €', ' Euro'),
+                        price: replaceNonSupportedChars(CentToEuro(preisliste.PauschalKosten[key].Preis)),
                     })
                 }
 
@@ -83,10 +100,10 @@ function generateJson(Guests, preisliste, mainconfig) {
                     items_used.map((item) => {
                         sum_cost += Number(item.price_sum);
                         Items_Array.push({
-                            artikel: item.produktname.substring(0, 32),
-                            priceper: CentToEuro(item.price_per_item).replace(' €', ' Euro'),
+                            artikel: replaceNonSupportedChars(item.produktname.substring(0, 32)),
+                            priceper: replaceNonSupportedChars(CentToEuro(item.price_per_item)),
                             amount: `${item.bough}x`,
-                            price: CentToEuro(item.price_sum).replace(' €', ' Euro'),
+                            price: replaceNonSupportedChars(CentToEuro(item.price_sum)),
                         })
                     })
                 }
